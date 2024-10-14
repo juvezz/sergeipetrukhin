c72b91eda40ac0ad0c8f70eb0909bd2abfb544ea
juvezz <petrukhin.s.a@gmail.com>
2024-10-14 14:57:02 +0200

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './globals.css'; // If you have global styles
+import { Analytics } from "@vercel/analytics/react"
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow">
+          {children}
+          <Analytics />
+        </main>
         <Footer />
       </body>
     </html>

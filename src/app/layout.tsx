a297864b2f69b48ca7d5b5eaf12c7aee455c8a77
juvezz <petrukhin.s.a@gmail.com>
2024-10-23 13:33:49 +0200

    add google meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="WuU1ZRYVUPEzdJtofU2IrpufmOyvGoj83_xfFd9gZY4" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">

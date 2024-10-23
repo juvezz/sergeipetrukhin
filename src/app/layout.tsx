164b2dff52e8a83074d5180f766998830f9a248a
juvezz <petrukhin.s.a@gmail.com>
2024-10-23 15:46:02 +0200

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './globals.css'; // If you have global styles
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from 'next'; // Import Metadata from next
+import Script from 'next/script'; // Import Script from next for adding scripts
 
 // Define metadata for the layout
 export const metadata: Metadata = {
@@ -19,7 +20,21 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <head>
-      <meta name="google-site-verification" content="WuU1ZRYVUPEzdJtofU2IrpufmOyvGoj83_xfFd9gZY4" />
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y14L1NNKN0"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Y14L1NNKN0');
+          `}
+        </Script>
+        
+        <meta name="google-site-verification" content="WuU1ZRYVUPEzdJtofU2IrpufmOyvGoj83_xfFd9gZY4" />
       </head>
       <body className="flex flex-col min-h-screen">
         <Header />

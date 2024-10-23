5f4d002c368edcd16ea385e22639d4a4edf5197f
juvezz <petrukhin.s.a@gmail.com>
2024-10-23 11:44:15 +0200

    add meta to layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,18 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import './globals.css'; // If you have global styles
 import { Analytics } from "@vercel/analytics/react"
+import { Metadata } from 'next'; // Import Metadata from next
+
+// Define metadata for the layout
+export const metadata: Metadata = {
+  title: "Blog about Testing", // Replace with your page title
+  description: "Different approaches for manual, automation or vulnerability testing", // Replace with your page description
+  keywords: ["QA", "qauality assurance", "automation", "sergeipetrukhin", "Selenium", "playwright"], // Optional: Add relevant keywords
+  robots: {
+    index: true,
+    follow: true,
+  }
+};
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (

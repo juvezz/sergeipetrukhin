d888d97e179cd757e52e6b1b41da4cc6ea45802f
juvezz <petrukhin.s.a@gmail.com>
2024-11-18 11:58:01 +0100

    fix github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,14 @@
-// playwright.config.ts
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'src/tests', // path to your tests directory
   use: {
     baseURL: 'http://localhost:3000',
-    headless: true,
+    headless: true, // Ensures the browser runs in headless mode
+    launchOptions: {
+      args: ['--disable-gpu', '--no-sandbox'], // Disable GPU and no sandbox for CI
+    },
+    browserName: 'chromium', // Make sure you're using chromium
   },
   webServer: {
     command: 'npm run dev',
@@ -14,8 +17,7 @@ export default defineConfig({
   },
   timeout: 30000,
   expect: {
-    timeout: 5000
+    timeout: 5000,
   },
   reporter: [['html', { open: 'on-failure' }]]
-  // More configurations can go here
 });
